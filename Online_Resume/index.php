228c5409d0d654792c2b0486d6c219d28fb4e32e
kincentino <38491580+kincentino@users.noreply.github.com>
2018-04-18 17:00:50 +0800

    Add files via upload

---
+++ Online_Resume/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Online Resume</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">	
+	<link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900|Cormorant+Garamond:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<a href="index.php" class="name">Mark Alvin Centino</a>
+		<nav>
+			<ul>
+				<li><a href="">About Me</a></li>
+				<li><a href="">Education</a></li>
+				<li><a href="">Background</a></li>			
+			</ul>
+			<a href="" class="projects">Projects Created</a>
+		</nav>
+	</header>
+
+	<main>
+		
+				<section class="index-banner">
+					<div class="vertical-center">
+						<h2>I AM A FREELANCE <br>WEB & MOBILE<br>DEVELOPER</h2>
+					<h1>With specialty in back-end development, functionality and UI/UX design.</h1>
+					</div>			
+				</section>
+
+				<div class="wrapper">
+				<section>
+					<!-- <img class="mypic" src="image/me.jpg" alt="standing man"> -->
+					<div class="container">
+						
+						<article class="aboutme">
+							<h3>About Me!</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a orci eu nibh fermentum aliquet. Pellentesque tempus lacus at dui sodales fermentum. Nunc bibendum, nulla a posuere dapibus, sem ipsum gravida diam, ut vestibulum leo orci eget metus. Nullam porttitor tortor vitae velit molestie, non consequat enim sagittis. Aliquam et tempus quam, nec hendrerit metus. Vestibulum venenatis consequat sollicitudin. Etiam magna augue, euismod vitae cursus in, porta interdum odio. Nunc interdum mollis sapien nec gravida. Praesent elementum mauris libero, et tempus ex imperdiet ac.</p>
+						</article>
+						<div class="mypic"></div>
+					</div>
+				</section>
+		</main>
+			<footer>
+				<ul>
+					<li><a href="">About Me</a></li>
+					<li><a href="">Education</a></li>
+					<li><a href="">Background</a></li>			
+				</ul>
+				<div class="footer-img">
+					<a href="https://www.facebook.com/MarkAlvinCentino" target="_blank"><img src="image/facebook.jpg"></a>
+					<a href="https://www.instagram.com/markalvincentino/" target="_blank"><img src="image/instagram.png"></a>
+				</div>
+			</footer>
+	</div>
+</body>
+</html>
